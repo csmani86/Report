fb0190505ca4b7085ebadf3e4805d663cee372e5
Manikandan <manikandan.c@skava.com>
2018-07-28 07:59:22 +0530

    Browser

--- src/main/java/com/framework/reporting/BaseClass.java
+++ src/main/java/com/framework/reporting/BaseClass.java
@@ -201,6 +201,7 @@ public class BaseClass
 			}
 			
 			System.out.println("Suite Name : " + System.getProperty("suiteXmlFile"));
+			System.out.println("Browser Name : " + System.getProperty("currentBrowser"));
 			
 			//Read test runner file for test run configurations
 			initBatchExec();
